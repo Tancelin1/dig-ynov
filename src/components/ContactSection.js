88108ce4e3aace6253d91519df41cd525ead3918
tancelin <tancelinnavez@outlook.fr>
2025-12-17 12:07:53 +0100

    chance contact style

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,14 +1,18 @@
+import '../assets/ContactSection.css';
+
 function ContactSection() {
   return (
     <section className="contact-section">
       <h2>Contact</h2>
 
-      <form className="contact-form">
-        <input type="email" placeholder="Votre email" />
-        <textarea placeholder="Votre message"></textarea>
-
-        <button type="submit">Envoyer</button>
-      </form>
+      <div className="contact-form">
+  <p>
+    Vous pouvez nous contacter via l'adresse mail suivante : 
+    <a href="mailto:digynov@gmail.com" className="email-link">
+      digynov@gmail.com
+    </a>
+  </p>
+</div>
     </section>
   );
 }
